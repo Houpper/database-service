5e7bfcf1c4eda33d79078c638f4002fabd6b7321
Fernando Kramer De Souza <souza.kramer.fernando@outlook.com>
2026-09-17 23:14:02 -0300

    Serviço para gerenciamento do banco de dados criado, incluído as principais configurações iniciais, Incluído setup Docker configuração para se comunicar com Eureka

---
+++ src/main/java/br/com/houpper/database_service/DatabaseServiceApplication.java
@@ -0,0 +1,23 @@
+package br.com.houpper.database_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Aplicação responsável pelo gerenciamento do banco de dados. Gerencia a criação e atualização de schemas e a execução
+ * das migrações do banco de dados utilizando o Flyway.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DatabaseServiceApplication {
+
+	/**
+	 * Inicializa a aplicação de gerenciamento do banco de dados.
+	 *
+	 * @param args Os argumentos da linha de comando.
+	 */
+    static void main(String[] args) {
+        SpringApplication.run(DatabaseServiceApplication.class, args);
+    }
+}
